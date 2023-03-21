79d8f2712357b2b64e859fbc4766da39da8d9a20
Xueqi <1090650726@qq.com>
2023-03-21 23:16:38 +0800

    修复了首页顶部输入框呼出键盘时消失的bug

--- app/src/main/java/com/example/template/ui/MainActivity.java
+++ app/src/main/java/com/example/template/ui/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StatusBarUtil.setTransparentForImageView(this, null);
         initFragmentList();
     }
 
--- architecture/src/main/java/com/kunminx/architecture/ui/page/BaseActivity.java
+++ architecture/src/main/java/com/kunminx/architecture/ui/page/BaseActivity.java
@@ -45,7 +45,7 @@ public abstract class BaseActivity extends DataBindingActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         BarUtils.setStatusBarColor(this, Color.TRANSPARENT);
-        BarUtils.setStatusBarLightMode(this, true);
+//        BarUtils.setStatusBarLightMode(this, true);
 
         super.onCreate(savedInstanceState);
 
