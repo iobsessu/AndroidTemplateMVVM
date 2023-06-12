3d6036841480a45ca316daa7b3134b09eccc3417
Xueqi Sun <xueqi.sun@perkinelmer.com>
2023-06-12 17:46:12 +0800

    日常

--- app/src/main/java/com/example/template/ui/adapter/TestInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.template.ui.adapter;
-
-import com.example.template.ui.components.calendarview.GroupRecyclerAdapter;
-
-//public interface TestInterface extends GroupRecyclerAdapter {
-//}
--- app/src/main/java/com/example/template/util/ResourceUtil.java
+++ app/src/main/java/com/example/template/util/ResourceUtil.java
@@ -1,6 +1,11 @@
 package com.example.template.util;
 
+import android.content.Context;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
+import androidx.core.content.ContextCompat;
 
 import com.example.template.app.AppApplication;
 
@@ -8,6 +13,10 @@ public class ResourceUtil {
 
     public static String getString(@StringRes int resId) {
         return AppApplication.getInstance().getString(resId);
+    }
+
+    static int getColor(@ColorRes int color) {
+        return AppApplication.getInstance().getColor(color);
     }
 
 }
