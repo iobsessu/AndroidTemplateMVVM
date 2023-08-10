eafdac2d99cc253aac1e64701e5af156d7a78b3f
Xueqi <1090650726@qq.com>
2023-08-10 22:29:33 +0800

    补充了一些

--- app/src/main/java/com/example/template/util/FormUtil.java
+++ app/src/main/java/com/example/template/util/FormUtil.java
@@ -7,6 +7,7 @@ import com.example.template.data.bean.FormItem;
 import com.example.template.data.bean.Staff;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class FormUtil {
@@ -18,14 +19,50 @@ public class FormUtil {
         formItem = new FormItem(ResUtil.getString(R.string.gender), formatGender(staff.getGender()), "gender");
         formItem.setType(FormItem.SINGLE_SELECT);
         list.add(formItem);
-        formItem = new FormItem(ResUtil.getString(R.string.birth_date), MyDateUtil.format(staff.getBirthDate()), "birthDate");
+        formItem = new FormItem(ResUtil.getString(R.string.birth_date), appendBirthDateStr(staff.getBirthDate()), "birthDate");
         formItem.setType(FormItem.DATE);
         list.add(formItem);
-        formItem = new FormItem(ResUtil.getString(R.string.age), staff.getBirthDate() +"", "birthDate");
+        formItem = new FormItem(ResUtil.getString(R.string.id_number), staff.getIdNumber(), "idNumber");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.phone_number), staff.getPhoneNumber(), "phoneNumber");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.email), staff.getEmail(), "email");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.address), staff.getAddress(), "address");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.department_name), staff.getDepartmentName(), "departmentName");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.position_name), staff.getPositionName(), "positionName");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.job_number), staff.getJobNumber(), "jobNumber");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.hire_date), MyDateUtil.format(staff.getHireDate()), "hireDate");
         formItem.setType(FormItem.DATE);
         list.add(formItem);
-
+        formItem = new FormItem(ResUtil.getString(R.string.leave_date), MyDateUtil.format(staff.getLeaveDate()), "leaveDate");
+        formItem.setType(FormItem.DATE);
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.work_location), staff.getWorkLocation(), "workLocation");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.working_status), (staff.isWorkingStatus()) + "", "workingStatus");
+        formItem.setType(FormItem.SINGLE_SELECT);
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.weekly_hours), staff.getWeeklyWorkingHours() + "", "weeklyWorkingHours");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.salary), staff.getSalary() + "", "salary");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.annual_leave_day), staff.getAnnualLeaveDays() + "", "annualLeaveDays");
+        list.add(formItem);
+        formItem = new FormItem(ResUtil.getString(R.string.health_status), staff.getHealthStatus() + "", "healthStatus");
+        list.add(formItem);
         return list;
+    }
+
+    public static String appendBirthDateStr(Date birthDate) {
+        if (birthDate == null) return "";
+        String birthDateStr = MyDateUtil.format(birthDate);
+        String ageStr = MyDateUtil.getAge(birthDate);
+        return birthDateStr + "（" + ageStr + "）";
     }
 
     public static String formatGender(int gender) {
